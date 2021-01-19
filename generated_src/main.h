faf886d098a8bf37666cfad38519eb662ff0ce19
ikhefacha <52214334+ikhefacha@users.noreply.github.com>
2021-01-19 20:47:22 +0100

    Delete generated_src directory

--- generated_src/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-/* Main mutex */
-extern pthread_mutex_t __mainMutex;
-
-/* Synchronous channels */
-extern syncchannel __System_chin__System_chout;
-/* Asynchronous channels */
-#endif
